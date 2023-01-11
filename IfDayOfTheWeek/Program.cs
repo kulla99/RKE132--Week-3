31ea1d2aa52749ee112434defc27496799bb6117
kulla <kujakobs@tktk.ee>
2023-01-11 18:54:25 +0200

    add week3 code

---
+++ IfDayOfTheWeek/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+if(weekDay == 0)
+{
+    Console.WriteLine("It´s Sunday!");
+}
+else if(weekDay == 1)
+{
+    Console.WriteLine("Its Monday!");
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("Its Tuesday");
+}
+else if (weekDay == 3)
+{
+    Console.WriteLine("its wendsday");
+}
+else if (weekDay == 4)
+{
+    Console.WriteLine("its thursday");
+}
+else if (weekDay == 5)
+{
+    Console.WriteLine("its friday");
+}
+else
+{
+    Console.WriteLine("its saturday");
+}    
+Console.WriteLine("have a nice day");
+
---
+++ switchDayOfTheWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek); //0-6
+
+switch (weekDay)
+{
+    case 0:
+        Console.WriteLine("its sunday");
+        break;
+    case 1:
+        Console.WriteLine("its monday");
+        break;
+    case 2:
+        Console.WriteLine("its tuesday");
+        break;
+    case 3:
+        Console.WriteLine("its wendsday");
+        break;
+    case 4:
+        Console.WriteLine("its thursday");
+        break;
+    case 5:
+        Console.WriteLine("its friday");
+        break;
+    case 6:
+        Console.WriteLine("its saturday");
+        break;
+    default:
+        Console.WriteLine("Oops, your caander must be broken");
+        break;
+}
+
+Console.WriteLine("have a nice day!");
